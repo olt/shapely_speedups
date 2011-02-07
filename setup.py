514f85fd15ed78097a0af10a76ae4cf6a9664b11
Oliver Tonnhofer <olt@bogosoft.com>
2011-02-07 12:55:42 +0100

    add packages to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
 ext_modules = [Extension("shapely_speedups.speedups", ["shapely_speedups/speedups.c"], libraries=['geos_c'])]
@@ -7,6 +7,7 @@ setup_args = dict(
     name = 'shapely_speedups',
     long_description = open('README.md').read(),
     license = 'BSD',
+    packages = find_packages(),
     version = '0.1dev',
     ext_modules = ext_modules,
     )
